939079f9d34d5378b5957d2cea570dd7d73a6366
johnson <johnson@github.com>
2021-06-25 11:39:07 +0800

    2021-06-25_11:39

---
+++ README_myexample.md
@@ -0,0 +1,31 @@
+# 测试中文的多任务模型
+## 任务1， Aspect的情感分析
+
+1. 生成规范式的数据
+```buildoutcfg
+python experiments/myexample/mydata_prepro.py
+# tree data_my/
+data_my/
+└── canonical_data
+    ├── absa_dev.tsv
+    ├── absa_test.tsv
+    └── absa_train.tsv
+```
+2. 预处理数据为tokenizer后的格式, my_task_def.yml中定义的所有任务，都会进行处理
+```buildoutcfg
+python prepro_std.py --model bert-base-chinese --root_dir data_my/canonical_data --task_def experiments/myexample/my_task_def.yml --do_lower_case
+
+#tree data_my/canonical_data/
+data_my/canonical_data/
+├── absa_dev.tsv
+├── absa_test.tsv
+├── absa_train.tsv
+└── bert-base-chinese
+    ├── absa_dev.json
+    ├── absa_test.json
+    └── absa_train.json
+```
+3. 训练任务
+```buildoutcfg
+python train.py --init_checkpoint mt_dnn_models/bert_model_base_chinese.pt --task_def experiments/myexample/my_task_def.yml --data_dir data_my/canonical_data/bert-base-chinese --train_datasets absa --test_datasets absa```
+```
--- experiments/myexample/mydata_prepro.py
+++ experiments/myexample/mydata_prepro.py
@@ -8,10 +8,17 @@ from experiments.common_utils import dump_rows
 from data_utils.task_def import DataFormat
 from data_utils.log_wrapper import create_logger
 import sys
+
+logger = create_logger(__name__, to_disk=True, log_file='mydata_prepro.log')
+
 sys.path.append('/Users/admin/git/TextBrewer/huazhuang/utils')
 from convert_label_studio_data import get_all_and_weibo_75, get_all, get_demision8, do_truncate_data
 
-logger = create_logger(__name__, to_disk=True, log_file='mydata_prepro.log')
+absa_labels2id = {
+    "消极":0,
+    "中性":1,
+    "积极":2,
+}
 
 def load_absa(train_rate=0.8, dev_rate=0.1, test_rate=0.1, left_max_seq_len=50, aspect_max_seq_len=8, right_max_seq_len=50):
     """
@@ -23,7 +30,7 @@ def load_absa(train_rate=0.8, dev_rate=0.1, test_rate=0.1, left_max_seq_len=50, 
     # get_demision8(save_path='../data_root_dir/demision8',
     #               dirpath_list=['/opt/lavector/effect', '/opt/lavector/pack', '/opt/lavector/promotion',
     #                             '/opt/lavector/component', '/opt/lavector/fragrance'])
-    all_data = get_all(dirpath=f"/opt/lavector/absa", do_save=False)
+    all_data = get_all(split=False, dirpath=f"/opt/lavector/absa", do_save=False)
     original_data, data, locations = do_truncate_data(all_data,left_max_seq_len, aspect_max_seq_len, right_max_seq_len)
     random.seed(30)
     random.shuffle(data)
@@ -38,8 +45,9 @@ def load_absa(train_rate=0.8, dev_rate=0.1, test_rate=0.1, left_max_seq_len=50, 
     def change_data(kind_data):
         rows = []
         for idx, one_data in enumerate(kind_data):
-            content, keyword, start_idx, stop_idx, sentiment, channel, word_type = one_data
-            sample = {'uid': idx, 'premise': content, 'hypothesis': keyword, 'label': sentiment}
+            content, keyword, sentiment = one_data
+            label_id = absa_labels2id[sentiment]
+            sample = {'uid': idx, 'premise': content, 'hypothesis': keyword, 'label': label_id}
             rows.append(sample)
         return rows
     absa_train_data = change_data(train_data)
@@ -50,15 +58,14 @@ def load_absa(train_rate=0.8, dev_rate=0.1, test_rate=0.1, left_max_seq_len=50, 
 def parse_args():
     parser = argparse.ArgumentParser(description='Preprocessing GLUE/SNLI/SciTail dataset.')
     parser.add_argument('--seed', type=int, default=13)
-    parser.add_argument('--root_dir', type=str, default='data')
-    parser.add_argument('--old_glue', action='store_true', help='whether it is old GLUE, refer official GLUE webpage for details')
+    parser.add_argument('--root_dir', type=str, default='data_my')
     args = parser.parse_args()
     return args
 
 
 def main(args):
     root = args.root_dir
-    assert os.path.exists(root)
+    assert os.path.exists(root), f"运行的路径是系统的根目录吗，请确保{root}文件夹存在"
     canonical_data_suffix = "canonical_data"
     canonical_data_root = os.path.join(root, canonical_data_suffix)
     if not os.path.isdir(canonical_data_root):
@@ -67,13 +74,13 @@ def main(args):
     #保存成tsv文件
     absa_train_data, absa_dev_data, absa_test_data = load_absa()
     #保存文件
-    abas_train_fout = os.path.join(canonical_data_root, 'abas_train.tsv')
-    abas_dev_fout = os.path.join(canonical_data_root, 'abas_dev.tsv')
-    abas_test_fout = os.path.join(canonical_data_root, 'abas_test.tsv')
-    dump_rows(absa_train_data, abas_train_fout, DataFormat.PremiseAndOneHypothesis)
-    dump_rows(absa_dev_data, abas_dev_fout, DataFormat.PremiseAndOneHypothesis)
-    dump_rows(absa_test_data, abas_test_fout, DataFormat.PremiseAndOneHypothesis)
-    logger.info('初步处理absa数据完成')
+    absa_train_fout = os.path.join(canonical_data_root, 'absa_train.tsv')
+    absa_dev_fout = os.path.join(canonical_data_root, 'absa_dev.tsv')
+    absa_test_fout = os.path.join(canonical_data_root, 'absa_test.tsv')
+    dump_rows(absa_train_data, absa_train_fout, DataFormat.PremiseAndOneHypothesis)
+    dump_rows(absa_dev_data, absa_dev_fout, DataFormat.PremiseAndOneHypothesis)
+    dump_rows(absa_test_data, absa_test_fout, DataFormat.PremiseAndOneHypothesis)
+    logger.info(f'初步处理absa数据完成, 保存规范后的数据到{absa_train_fout}, {absa_dev_fout}, {absa_test_fout}')
 
 if __name__ == '__main__':
     args = parse_args()
