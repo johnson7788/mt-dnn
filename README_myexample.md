ff7419c28df625d6fb915c944667bd162b8a38e2
johnson <johnson@github.com>
2021-06-25 12:52:59 +0800

    2021-06-25_12:52

--- README_myexample.md
+++ README_myexample.md
@@ -1,5 +1,6 @@
 # 测试中文的多任务模型
-## 任务1， Aspect的情感分析
+## 任务1， Aspect的情感分析，3分类
+## 任务2， 8个维度的词性判别，2分类
 
 1. 生成规范式的数据
 ```buildoutcfg
@@ -9,21 +10,31 @@ data_my/
 └── canonical_data
     ├── absa_dev.tsv
     ├── absa_test.tsv
-    └── absa_train.tsv
+    ├── absa_train.tsv
+    ├── dem8_dev.tsv
+    ├── dem8_test.tsv
+    └── dem8_train.tsv
 ```
 2. 预处理数据为tokenizer后的格式, my_task_def.yml中定义的所有任务，都会进行处理
 ```buildoutcfg
 python prepro_std.py --model bert-base-chinese --root_dir data_my/canonical_data --task_def experiments/myexample/my_task_def.yml --do_lower_case
 
 #tree data_my/canonical_data/
-data_my/canonical_data/
-├── absa_dev.tsv
-├── absa_test.tsv
-├── absa_train.tsv
-└── bert-base-chinese
-    ├── absa_dev.json
-    ├── absa_test.json
-    └── absa_train.json
+data_my/
+└── canonical_data
+    ├── absa_dev.tsv
+    ├── absa_test.tsv
+    ├── absa_train.tsv
+    ├── bert-base-chinese
+    │   ├── absa_dev.json
+    │   ├── absa_test.json
+    │   ├── absa_train.json
+    │   ├── dem8_dev.json
+    │   ├── dem8_test.json
+    │   └── dem8_train.json
+    ├── dem8_dev.tsv
+    ├── dem8_test.tsv
+    └── dem8_train.tsv
 ```
 3. 训练任务
 ```buildoutcfg
--- experiments/myexample/mydata_prepro.py
+++ experiments/myexample/mydata_prepro.py
@@ -14,24 +14,42 @@ logger = create_logger(__name__, to_disk=True, log_file='mydata_prepro.log')
 sys.path.append('/Users/admin/git/TextBrewer/huazhuang/utils')
 from convert_label_studio_data import get_all_and_weibo_75, get_all, get_demision8, do_truncate_data
 
-absa_labels2id = {
-    "消极":0,
-    "中性":1,
-    "积极":2,
-}
-
-def load_absa(train_rate=0.8, dev_rate=0.1, test_rate=0.1, left_max_seq_len=50, aspect_max_seq_len=8, right_max_seq_len=50):
+def load_absa_dem8(kind='absa', train_rate=0.8, dev_rate=0.1, test_rate=0.1, left_max_seq_len=50, aspect_max_seq_len=8, right_max_seq_len=50):
     """
     Aspect Base sentiment analysis
+    :param kind: 是加载absa数据，还是dem8的数据
     :return:
     :rtype:
     """
+    if kind == 'absa':
+        all_data = get_all(split=False, dirpath=f"/opt/lavector/absa", do_save=False)
+        labels2id = {
+            "消极": 0,
+            "中性": 1,
+            "积极": 2,
+        }
+    elif kind == 'dem8':
         # 注意dirpath_list使用哪些数据进行训练，那么预测时，也是用这样的数据
-    # get_demision8(save_path='../data_root_dir/demision8',
-    #               dirpath_list=['/opt/lavector/effect', '/opt/lavector/pack', '/opt/lavector/promotion',
-    #                             '/opt/lavector/component', '/opt/lavector/fragrance'])
-    all_data = get_all(split=False, dirpath=f"/opt/lavector/absa", do_save=False)
+        labels2id = {
+            "是": 0,
+            "否": 1,
+        }
+        all_data = get_demision8(split=False, dirpath_list=['/opt/lavector/effect', '/opt/lavector/pack', '/opt/lavector/promotion','/opt/lavector/component', '/opt/lavector/fragrance'])
+    else:
+        print("数据的种类不存在，退出")
+        sys.exit(1)
     original_data, data, locations = do_truncate_data(all_data,left_max_seq_len, aspect_max_seq_len, right_max_seq_len)
+    if kind == 'dem8':
+        #处理完成的数据加前缀
+        # 加上前缀，给每条数据
+        new_data = []
+        for dall, d in zip(all_data, data):
+            word_type = dall[6]
+            content = word_type + ':' + d[0]
+            new_one = list(d)
+            new_one[0] = content
+            new_data.append(new_one)
+        data = new_data
     random.seed(30)
     random.shuffle(data)
     total = len(data)
@@ -46,7 +64,7 @@ def load_absa(train_rate=0.8, dev_rate=0.1, test_rate=0.1, left_max_seq_len=50, 
         rows = []
         for idx, one_data in enumerate(kind_data):
             content, keyword, sentiment = one_data
-            label_id = absa_labels2id[sentiment]
+            label_id = labels2id[sentiment]
             sample = {'uid': idx, 'premise': content, 'hypothesis': keyword, 'label': label_id}
             rows.append(sample)
         return rows
@@ -54,6 +72,7 @@ def load_absa(train_rate=0.8, dev_rate=0.1, test_rate=0.1, left_max_seq_len=50, 
     absa_dev_data = change_data(dev_data)
     absa_test_data = change_data(test_data)
     return absa_train_data, absa_dev_data, absa_test_data
+
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Preprocessing GLUE/SNLI/SciTail dataset.')
@@ -71,8 +90,10 @@ def main(args):
     if not os.path.isdir(canonical_data_root):
         os.mkdir(canonical_data_root)
     print(f"将要保存到{canonical_data_root}")
+
+    ##############ABSA 的数据##############
     #保存成tsv文件
-    absa_train_data, absa_dev_data, absa_test_data = load_absa()
+    absa_train_data, absa_dev_data, absa_test_data = load_absa_dem8(kind='absa')
     #保存文件
     absa_train_fout = os.path.join(canonical_data_root, 'absa_train.tsv')
     absa_dev_fout = os.path.join(canonical_data_root, 'absa_dev.tsv')
@@ -81,6 +102,17 @@ def main(args):
     dump_rows(absa_dev_data, absa_dev_fout, DataFormat.PremiseAndOneHypothesis)
     dump_rows(absa_test_data, absa_test_fout, DataFormat.PremiseAndOneHypothesis)
     logger.info(f'初步处理absa数据完成, 保存规范后的数据到{absa_train_fout}, {absa_dev_fout}, {absa_test_fout}')
+
+    ##############8个维度的数据##############
+    dem8_train_data, dem8_dev_data, dem8_test_data = load_absa_dem8(kind='dem8')
+    #保存文件
+    dem8_train_fout = os.path.join(canonical_data_root, 'dem8_train.tsv')
+    dem8_dev_fout = os.path.join(canonical_data_root, 'dem8_dev.tsv')
+    dem8_test_fout = os.path.join(canonical_data_root, 'dem8_test.tsv')
+    dump_rows(dem8_train_data, dem8_train_fout, DataFormat.PremiseAndOneHypothesis)
+    dump_rows(dem8_dev_data, dem8_dev_fout, DataFormat.PremiseAndOneHypothesis)
+    dump_rows(dem8_test_data, dem8_test_fout, DataFormat.PremiseAndOneHypothesis)
+    logger.info(f'初步处理dem8数据完成, 保存规范后的数据到{dem8_train_fout}, {dem8_dev_fout}, {dem8_test_fout}')
 
 if __name__ == '__main__':
     args = parse_args()
