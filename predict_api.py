d7bd03b39646e3c6255bb6c4a9753c6b36e919e2
johnson <johnson@github.com>
2021-06-30 15:56:39 +0800

    2021-06-30_15:56

--- predict_api.py
+++ predict_api.py
@@ -66,7 +66,7 @@ class SinglePredictDataset(Dataset):
                  max_seq_length=512,
                  max_predictions_per_seq=80,
                  printable=True):
-        data = self.build_data(data=data,tokenizer=tokenizer, data_format=task_def.data_type, lab_dict=task_def.label_vocab)
+        data = self.build_data(data=data, tokenizer=tokenizer, data_format=task_def.data_type, lab_dict=task_def.label_vocab)
         data, tokenizer = self.load(data, is_train, maxlen, factor, task_def, printable=printable)
         self._data = data
         self._tokenizer = tokenizer
@@ -85,8 +85,7 @@ class SinglePredictDataset(Dataset):
     def get_task_id(self):
         return self._task_id
 
-    @staticmethod
-    def load(path, is_train=True, maxlen=512, factor=1.0, task_def=None, printable=True):
+    def load(self, path, is_train=True, maxlen=512, factor=1.0, task_def=None, printable=True):
         task_type = task_def.task_type
         assert task_type is not None
         with open(path, 'r', encoding='utf-8') as reader:
@@ -109,8 +108,7 @@ class SinglePredictDataset(Dataset):
                 print('Loaded {} samples out of {}'.format(len(data), cnt))
         return data, None
 
-    @staticmethod
-    def feature_extractor(tokenizer, text_a, text_b=None, max_length=512, do_padding=False):
+    def feature_extractor(self, tokenizer, text_a, text_b=None, max_length=512, do_padding=False):
         inputs = tokenizer(
             text_a,
             text_b,
@@ -133,170 +131,174 @@ class SinglePredictDataset(Dataset):
                                                                                                 max_length)
         return input_ids, attention_mask, token_type_ids
 
-    @staticmethod
-    def build_data(data, tokenizer, data_format=DataFormat.PremiseOnly,
+    def build_data(self, data, tokenizer, data_format=DataFormat.PremiseOnly,
                    max_seq_len=512, lab_dict=None, do_padding=False, truncation=True):
         def build_data_premise_only(
-                data, dump_path, max_seq_len=512, tokenizer=None):
+                data, max_seq_len=512, tokenizer=None):
             """Build data of single sentence tasks
             """
-            with open(dump_path, 'w', encoding='utf-8') as writer:
-                for idx, sample in enumerate(data):
-                    ids = sample['uid']
-                    premise = sample['premise']
-                    label = sample['label']
-                    input_ids, input_mask, type_ids = self.feature_extractor(tokenizer, premise, max_length=max_seq_len)
-                    features = {
-                        'uid': ids,
-                        'label': label,
-                        'token_id': input_ids,
-                        'type_id': type_ids,
-                        'attention_mask': input_mask}
-                    writer.write('{}\n'.format(json.dumps(features)))
+            feature_datas = []
+            for idx, sample in enumerate(data):
+                ids = sample['uid']
+                premise = sample['premise']
+                label = sample['label']
+                input_ids, input_mask, type_ids = self.feature_extractor(tokenizer, premise, max_length=max_seq_len)
+                features = {
+                    'uid': ids,
+                    'label': label,
+                    'token_id': input_ids,
+                    'type_id': type_ids,
+                    'attention_mask': input_mask}
+                feature_datas.append(features)
+            return feature_datas
 
         def build_data_premise_and_one_hypo(
                 data, dump_path, max_seq_len=512, tokenizer=None):
             """Build data of sentence pair tasks
             """
-            with open(dump_path, 'w', encoding='utf-8') as writer:
-                for idx, sample in enumerate(data):
-                    ids = sample['uid']
-                    premise = sample['premise']
-                    hypothesis = sample['hypothesis']
-                    label = sample['label']
-                    input_ids, input_mask, type_ids = self.feature_extractor(tokenizer, premise, text_b=hypothesis,
-                                                                        max_length=max_seq_len)
-                    features = {
-                        'uid': ids,
-                        'label': label,
-                        'token_id': input_ids,
-                        'type_id': type_ids,
-                        'attention_mask': input_mask}
-                    writer.write('{}\n'.format(json.dumps(features)))
+            feature_datas = []
+            for idx, sample in enumerate(data):
+                ids = sample['uid']
+                premise = sample['premise']
+                hypothesis = sample['hypothesis']
+                label = sample['label']
+                input_ids, input_mask, type_ids = self.feature_extractor(tokenizer, premise, text_b=hypothesis,
+                                                                    max_length=max_seq_len)
+                features = {
+                    'uid': ids,
+                    'label': label,
+                    'token_id': input_ids,
+                    'type_id': type_ids,
+                    'attention_mask': input_mask}
+                feature_datas.append(features)
+            return feature_datas
 
         def build_data_premise_and_multi_hypo(
-                data, dump_path, max_seq_len=512, tokenizer=None):
+                data, max_seq_len=512, tokenizer=None):
             """Build QNLI as a pair-wise ranking task
             """
-            with open(dump_path, 'w', encoding='utf-8') as writer:
-                for idx, sample in enumerate(data):
-                    ids = sample['uid']
-                    premise = sample['premise']
-                    hypothesis_list = sample['hypothesis']
-                    label = sample['label']
-                    input_ids_list = []
-                    type_ids_list = []
-                    attention_mask_list = []
-                    for hypothesis in hypothesis_list:
-                        input_ids, input_mask, type_ids = self.feature_extractor(tokenizer,
-                                                                            premise, hypothesis, max_length=max_seq_len)
-                        input_ids_list.append(input_ids)
-                        type_ids_list.append(type_ids)
-                        attention_mask_list.append(input_mask)
-                    features = {
-                        'uid': ids,
-                        'label': label,
-                        'token_id': input_ids_list,
-                        'type_id': type_ids_list,
-                        'ruid': sample['ruid'],
-                        'olabel': sample['olabel'],
-                        'attention_mask': attention_mask_list}
-                    writer.write('{}\n'.format(json.dumps(features)))
+            feature_datas = []
+            for idx, sample in enumerate(data):
+                ids = sample['uid']
+                premise = sample['premise']
+                hypothesis_list = sample['hypothesis']
+                label = sample['label']
+                input_ids_list = []
+                type_ids_list = []
+                attention_mask_list = []
+                for hypothesis in hypothesis_list:
+                    input_ids, input_mask, type_ids = self.feature_extractor(tokenizer,
+                                                                        premise, hypothesis, max_length=max_seq_len)
+                    input_ids_list.append(input_ids)
+                    type_ids_list.append(type_ids)
+                    attention_mask_list.append(input_mask)
+                features = {
+                    'uid': ids,
+                    'label': label,
+                    'token_id': input_ids_list,
+                    'type_id': type_ids_list,
+                    'ruid': sample['ruid'],
+                    'olabel': sample['olabel'],
+                    'attention_mask': attention_mask_list}
+                feature_datas.append(features)
+            return feature_datas
 
         def build_data_sequence(data, dump_path, max_seq_len=512, tokenizer=None, label_mapper=None):
-            with open(dump_path, 'w', encoding='utf-8') as writer:
-                for idx, sample in enumerate(data):
-                    ids = sample['uid']
-                    premise = sample['premise']
-                    tokens = []
-                    labels = []
-                    for i, word in enumerate(premise):
-                        subwords = tokenizer.tokenize(word)
-                        tokens.extend(subwords)
-                        for j in range(len(subwords)):
-                            if j == 0:
-                                labels.append(sample['label'][i])
-                            else:
-                                labels.append(label_mapper['X'])
-                    if len(premise) > max_seq_len - 2:
-                        tokens = tokens[:max_seq_len - 2]
-                        labels = labels[:max_seq_len - 2]
+            feature_datas = []
+            for idx, sample in enumerate(data):
+                ids = sample['uid']
+                premise = sample['premise']
+                tokens = []
+                labels = []
+                for i, word in enumerate(premise):
+                    subwords = tokenizer.tokenize(word)
+                    tokens.extend(subwords)
+                    for j in range(len(subwords)):
+                        if j == 0:
+                            labels.append(sample['label'][i])
+                        else:
+                            labels.append(label_mapper['X'])
+                if len(premise) > max_seq_len - 2:
+                    tokens = tokens[:max_seq_len - 2]
+                    labels = labels[:max_seq_len - 2]
 
-                    label = [label_mapper['CLS']] + labels + [label_mapper['SEP']]
-                    input_ids = tokenizer.convert_tokens_to_ids([tokenizer.cls_token] + tokens + [tokenizer.sep_token])
-                    assert len(label) == len(input_ids)
-                    type_ids = [0] * len(input_ids)
-                    features = {'uid': ids, 'label': label, 'token_id': input_ids, 'type_id': type_ids}
-                    writer.write('{}\n'.format(json.dumps(features)))
+                label = [label_mapper['CLS']] + labels + [label_mapper['SEP']]
+                input_ids = tokenizer.convert_tokens_to_ids([tokenizer.cls_token] + tokens + [tokenizer.sep_token])
+                assert len(label) == len(input_ids)
+                type_ids = [0] * len(input_ids)
+                features = {'uid': ids, 'label': label, 'token_id': input_ids, 'type_id': type_ids}
+                feature_datas.append(features)
+            return feature_datas
 
         def build_data_mrc(data, dump_path, max_seq_len=512, tokenizer=None, label_mapper=None, is_training=True):
-            with open(dump_path, 'w', encoding='utf-8') as writer:
-                unique_id = 1000000000  # TODO: this is from BERT, needed to remove it...
-                for example_index, sample in enumerate(data):
-                    ids = sample['uid']
-                    doc = sample['premise']
-                    query = sample['hypothesis']
-                    label = sample['label']
-                    doc_tokens, cw_map = squad_utils.token_doc(doc)
-                    answer_start, answer_end, answer, is_impossible = squad_utils.parse_squad_label(label)
-                    answer_start_adjusted, answer_end_adjusted = squad_utils.recompute_span(answer, answer_start,
-                                                                                            cw_map)
-                    is_valid = squad_utils.is_valid_answer(doc_tokens, answer_start_adjusted, answer_end_adjusted,
-                                                           answer)
-                    if not is_valid: continue
-                    """
-                    TODO --xiaodl: support RoBERTa
-                    """
-                    feature_list = squad_utils.mrc_feature(tokenizer,
-                                                           unique_id,
-                                                           example_index,
-                                                           query,
-                                                           doc_tokens,
-                                                           answer_start_adjusted,
-                                                           answer_end_adjusted,
-                                                           is_impossible,
-                                                           max_seq_len,
-                                                           512,
-                                                           180,
-                                                           answer_text=answer,
-                                                           is_training=True)
-                    unique_id += len(feature_list)
-                    for feature in feature_list:
-                        so = json.dumps({'uid': ids,
-                                         'token_id': feature.input_ids,
-                                         'mask': feature.input_mask,
-                                         'type_id': feature.segment_ids,
-                                         'example_index': feature.example_index,
-                                         'doc_span_index': feature.doc_span_index,
-                                         'tokens': feature.tokens,
-                                         'token_to_orig_map': feature.token_to_orig_map,
-                                         'token_is_max_context': feature.token_is_max_context,
-                                         'start_position': feature.start_position,
-                                         'end_position': feature.end_position,
-                                         'label': feature.is_impossible,
-                                         'doc': doc,
-                                         'doc_offset': feature.doc_offset,
-                                         'answer': [answer]})
-                        writer.write('{}\n'.format(so))
+            unique_id = 1000000000  # TODO: this is from BERT, needed to remove it...
+            feature_datas = []
+            for example_index, sample in enumerate(data):
+                ids = sample['uid']
+                doc = sample['premise']
+                query = sample['hypothesis']
+                label = sample['label']
+                doc_tokens, cw_map = squad_utils.token_doc(doc)
+                answer_start, answer_end, answer, is_impossible = squad_utils.parse_squad_label(label)
+                answer_start_adjusted, answer_end_adjusted = squad_utils.recompute_span(answer, answer_start,
+                                                                                        cw_map)
+                is_valid = squad_utils.is_valid_answer(doc_tokens, answer_start_adjusted, answer_end_adjusted,
+                                                       answer)
+                if not is_valid: continue
+                """
+                TODO --xiaodl: support RoBERTa
+                """
+                feature_list = squad_utils.mrc_feature(tokenizer,
+                                                       unique_id,
+                                                       example_index,
+                                                       query,
+                                                       doc_tokens,
+                                                       answer_start_adjusted,
+                                                       answer_end_adjusted,
+                                                       is_impossible,
+                                                       max_seq_len,
+                                                       512,
+                                                       180,
+                                                       answer_text=answer,
+                                                       is_training=True)
+                unique_id += len(feature_list)
+                for feature in feature_list:
+                    so = json.dumps({'uid': ids,
+                                     'token_id': feature.input_ids,
+                                     'mask': feature.input_mask,
+                                     'type_id': feature.segment_ids,
+                                     'example_index': feature.example_index,
+                                     'doc_span_index': feature.doc_span_index,
+                                     'tokens': feature.tokens,
+                                     'token_to_orig_map': feature.token_to_orig_map,
+                                     'token_is_max_context': feature.token_is_max_context,
+                                     'start_position': feature.start_position,
+                                     'end_position': feature.end_position,
+                                     'label': feature.is_impossible,
+                                     'doc': doc,
+                                     'doc_offset': feature.doc_offset,
+                                     'answer': [answer]})
+                    feature_datas.append(so)
+            return feature_datas
 
         if data_format == DataFormat.PremiseOnly:
             build_data_premise_only(
                 data,
-                dump_path,
                 max_seq_len,
                 tokenizer)
         elif data_format == DataFormat.PremiseAndOneHypothesis:
-            build_data_premise_and_one_hypo(
-                data, dump_path, max_seq_len, tokenizer)
+            feature_data = build_data_premise_and_one_hypo(
+                data, max_seq_len, tokenizer)
         elif data_format == DataFormat.PremiseAndMultiHypothesis:
-            build_data_premise_and_multi_hypo(
-                data, dump_path, max_seq_len, tokenizer)
+            feature_data = build_data_premise_and_multi_hypo(
+                data, max_seq_len, tokenizer)
         elif data_format == DataFormat.Seqence:
-            build_data_sequence(data, dump_path, max_seq_len, tokenizer, lab_dict)
+            feature_data = build_data_sequence(data, max_seq_len, tokenizer, lab_dict)
         elif data_format == DataFormat.MRC:
-            build_data_mrc(data, dump_path, max_seq_len, tokenizer)
+            feature_data = build_data_mrc(data, max_seq_len, tokenizer)
         else:
             raise ValueError(data_format)
+        return feature_data
 
     def __len__(self):
         return len(self._data)
@@ -348,7 +350,7 @@ class TorchMTDNNModel(object):
         self.max_seq_len = 512
         # 最大的batch_size
         self.predict_batch_size = 64
-        self.tokenize_model = 'bert-base-chinese',
+        self.tokenize_model = 'bert-base-chinese'
         # 训练好的模型的存放位置
         self.checkpoint = 'trained_model/absa_dem8.pt'
         # 是否放GPU
